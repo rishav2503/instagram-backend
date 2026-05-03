96dd03fe9e0bb4e1f8c8df4699dde264f2b52229
Rishav Roy <rishavroy25032005@gmail.com>
2026-05-03 18:22:10 +0530

    update

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.post("/create-post", authMiddleware, upload.single("image"), async (req, res
     const newPost = new Post({
       caption: req.body.caption,
       // Constructs the image URL based on the current host (ngrok or localhost)
-      image: `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`,
+      image: `${process.env.BASE_URL}/uploads/${req.file.filename}`,
       userId: req.user.userId
     });
 
